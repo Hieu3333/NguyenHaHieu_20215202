e18cc411867f02e94cf5aef7dfa0f9ec06c4d711
Hieu3333 <98133081+Hieu3333@users.noreply.github.com>
2023-05-15 14:49:08 +0700

    Add files via upload

---
+++ Aims.java
@@ -0,0 +1,32 @@
+package p;
+
+public class Aims {
+
+	public static void main(String[] args) {
+		Cart anOrder = new Cart();
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+				"Animation","Roger Allers",87, 19.95f);
+		anOrder.addDigitalVideoDisc(dvd1);
+		
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", 
+				"Science fiction", "George Lucas",87, 24.95f);
+		anOrder.addDigitalVideoDisc(dvd2);
+		
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
+		anOrder.addDigitalVideoDisc(dvd3);
+		
+		System.out.println("Total cost is: " + anOrder.totalCost());
+		
+		
+		anOrder.removeDigitalVideoDisc(dvd1);
+		
+		DigitalVideoDisc[] list = {dvd1,dvd2,dvd3};
+		anOrder.addDigitalVideoDisc(list);
+		anOrder.addDigitalVideoDisc(dvd1,dvd2);
+		anOrder.printOrder();
+		anOrder.searchID(1);
+		anOrder.searchTitle("Aladin");
+
+	}
+
+}
