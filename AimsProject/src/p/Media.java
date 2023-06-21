5632ae1795d0dbf087f0324af210280c80a02722
Hieu <Hieu30029081@gmail.com>
2023-06-21 14:22:54 +0700

    add equals exception

--- AimsProject/src/p/Media.java
+++ AimsProject/src/p/Media.java
@@ -67,12 +67,21 @@ public abstract class Media {
 	}
 
 	@Override
-	public boolean equals ( Object o) {
-		if (!(o instanceof Media)) {
-			return false;
-		}
-		Media a = (Media) o;
-		return (this.title == a.title);
+	public boolean equals(Object obj) {
+	    if (this == obj) {
+	        return true;
+	    }
+	    if (obj == null || getClass() != obj.getClass()) {
+	        return false;
+	    }
+	    Media otherMedia = (Media) obj;
+	    try {
+	        return title.equals(otherMedia.title);
+	    } catch (NullPointerException e) {
+	        return false;
+	    } catch (ClassCastException e) {
+	        return false;
+	    }
 	}
 
 
